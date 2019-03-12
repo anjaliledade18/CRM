bbdd443ff51aa7d0aec56ee36c38e92a0b5de9a5
anjaliledade18 <48476528+anjaliledade18@users.noreply.github.com>
2019-03-12 16:05:08 +0530

    Add files via upload

---
+++ completed_task.php
@@ -0,0 +1,17 @@
+Task completed:
+1)Registration form USING html and css
+
+2)Created Template using HTML and css.
+
+3)registration process for a student:
+  a)registartion page
+  b)login page
+  c)create task form
+  d)Retrive data from database to HTML tables task_created_name and task_name.
+
+4)Books and Author task-By creating relationship one to many and adding
+jquery(javascript) validation to phone numbers field.(It should be numeric upto digits)
+
+5)Created custom code for email template.
+
+6)When ticket will get created,by default it will send an email to assigned user and if ticket status will get closed,the mail will trigger to reporting manager using workflow.
